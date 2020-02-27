43d1b958fea2569dfdb924e174313fb4a17ff4c2
Patricio R <patricio.raschetti@gmail.com>
2020-02-27 15:44:58 -0300

    Layout and BurgerBuilder placeholdeers

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Layout from '../../hoc/Layout/Layout';
+import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
 
     render() {
         return (
             <>
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
             </>
         );
     };
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+
+    render() {
+        return (
+            <>
+                <div>
+                    Placeholder: Burger preview.
+                </div>
+                <div>
+                    Placeholder: Build controls.
+                </div>
+            </>
+        );
+    };
+};
+
+export default BurgerBuilder;
