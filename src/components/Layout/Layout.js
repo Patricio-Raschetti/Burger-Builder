6fd0f1536fc7c416b1b504860cb92fb574d784d9
Patricio R <patricio.raschetti@gmail.com>
2020-03-08 11:19:50 -0300

    Add Backdrop to SideDrawer; Layout is now stateful but limited.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
 import SideDrawer from './../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => (
-    <>
-        <Toolbar />
-        <SideDrawer />
-        <main className={styles.Content}>{props.children}</main>
-    </>
-);
+class Layout extends Component {
+    state = { showSideDrawer: true };
+
+    sideDrawerCloseHandler = () => {
+        this.setState({ showSideDrawer: false });
+    };
+
+    render() {
+        return (
+            <>
+                <Toolbar />
+                <SideDrawer opened={this.state.showSideDrawer} close={this.sideDrawerCloseHandler} />
+                <main className={styles.Content}>{this.props.children}</main>
+            </>
+        );
+    }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavBar from '../NavBar/NavBar';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import styles from './SideDrawer.module.css';
 
-const SideDrawer = props => {
-
-    return (
-        <div className={styles.SideDrawer}>
+const SideDrawer = props => (
+    <>
+        <Backdrop show={props.opened} clicked={props.close} />
+        <div className={`${styles.SideDrawer} ${props.opened ? styles.Open : styles.Close}`}>
             <div className={styles.Logo}>
                 <Logo />
             </div>
@@ -14,7 +15,7 @@ const SideDrawer = props => {
                 <NavBar />
             </nav>
         </div>
-    );
-};
+    </>
+);
 
 export default SideDrawer;
