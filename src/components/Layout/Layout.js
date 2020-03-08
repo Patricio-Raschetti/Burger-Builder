1f5da4dd13bd219d4503f5b6797423454f0074d5
Patricio R <patricio.raschetti@gmail.com>
2020-03-08 12:29:35 -0300

    Add DrawerToggle; Fix Logo bug.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,16 +4,20 @@ import styles from './Layout.module.css';
 import SideDrawer from './../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
-    state = { showSideDrawer: true };
+    state = { showSideDrawer: false };
 
     sideDrawerCloseHandler = () => {
         this.setState({ showSideDrawer: false });
     };
 
+    sideDrawerToggleHandler = () => {
+        this.setState(prevState => ({ showSideDrawer: !prevState.showSideDrawer }))
+    };
+
     render() {
         return (
             <>
-                <Toolbar />
+                <Toolbar drawerToggler={this.sideDrawerToggleHandler} />
                 <SideDrawer opened={this.state.showSideDrawer} close={this.sideDrawerCloseHandler} />
                 <main className={styles.Content}>{this.props.children}</main>
             </>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavBar from '../NavBar/NavBar';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const Toolbar = props => (
     <header className={styles.Toolbar}>
         <div className={styles.Logo}>
             <Logo />
         </div>
-        <div>Menu</div>
+        <DrawerToggle clicked={props.drawerToggler} />
         <nav className={styles.DesktopOnly}>
             <NavBar />
         </nav>
