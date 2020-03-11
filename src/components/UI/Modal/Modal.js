33d7aa4bd503775103151f9a8383975d340381b1
Patricio R <patricio.raschetti@gmail.com>
2020-03-11 20:17:51 -0300

    Add Modal optimization

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,23 +3,33 @@ import styles from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 import PropTypes from 'prop-types';
 
-const Modal = props => (
-    <>
-        <Backdrop
-            show={props.show}
-            clicked={props.modalClosed}
-        />
-        <div
-            className={styles.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? 1 : 0
-            }}
-        >
-            {props.children}
-        </div>
-    </>
-);
+const showAreEqual = (prev, next) => {
+    return prev.show === next.show;
+}
+
+const Modal = props => {
+    // useEffect(() => {
+    //     console.log('Modal rendered');
+    // });
+
+    return (
+        <>
+            <Backdrop
+                show={props.show}
+                clicked={props.modalClosed}
+            />
+            <div
+                className={styles.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? 1 : 0
+                }}
+            >
+                {props.children}
+            </div>
+        </>
+    );
+};
 
 Modal.propTypes = {
     show: PropTypes.bool.isRequired,
@@ -27,4 +37,4 @@ Modal.propTypes = {
     children: PropTypes.element.isRequired
 }
 
-export default Modal;
+export default React.memo(Modal, showAreEqual);
