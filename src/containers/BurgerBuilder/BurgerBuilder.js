8a6b12f4c41ba75897b08ad6e10a3aead19bac6b
Patricio R <patricio.raschetti@gmail.com>
2020-06-01 23:54:33 -0300

    Add withErrorHandler HOC for Http req error handling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,6 +8,8 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Modal from '../../components/UI/Modal/Modal';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -138,4 +140,4 @@ class BurgerBuilder extends Component {
     };
 };
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
